50d040c6edfa7c25d5c9c89077121619b4d9bf49
rhianlandim <rhiandl.moura@gmail.com>
2022-07-12 01:03:23 -0300

    finishing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import chuck_norris from "./assets/Chuck Norris.png";
+import { GiPistolGun }  from "react-icons/gi";
+import { useState } from "react";
+import api from "./services/api.js"
+
 
 function App() {
+
+const [ joke, setJoke ] = useState("")
+
+async function searchJoke () {
+
+  try {
+    const response = await api.get()
+    setJoke(response.data.value)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wanted">
+        <header className="title">
+          <h1>WANTED</h1>
+          <img src={chuck_norris}/>
+          <h2>----- Dead or Alive -----</h2>
+          <h1>Chuck Norris</h1>
+          <h2>---- $ 1000.000.000 ----</h2>
+        </header>
+      </div>
+
+        <div className="buttonArea">
+          <div className="area">
+            Press the button to see a joke.
+            <button onClick={searchJoke}>
+              <GiPistolGun size={35} color="#000"/>
+            </button>
+          </div>
+          <main className="main">
+            <span>{joke}</span>
+          </main>
+        </div>
     </div>
   );
 }
